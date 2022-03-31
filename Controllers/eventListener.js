d78db65a271f7cfefabbeb3daec54ca70e3bbb67
Maverick9081 <faliyaabhishek@gmail.com>
2022-03-31 11:40:30 +0530

    Changed the styling

--- Controllers/eventListener.js
+++ Controllers/eventListener.js
@@ -4,35 +4,37 @@ import Event from "../model/Events.js";
 import masterChef from "../ABI/masterChef.js";
 dotenv.config();
 
-    const alchemyApiKey = process.env.ALCHEMY_API;
-    const sushiMaster = process.env.SUSHIMASTER; 
-    const provider = new ethers.providers.WebSocketProvider(alchemyApiKey);
-    const contract = new ethers.Contract(sushiMaster,masterChef,provider);
+const alchemyApiKey = process.env.ALCHEMY_API;
+const sushiMaster = process.env.SUSHIMASTER; 
+const provider = new ethers.providers.WebSocketProvider(alchemyApiKey);
+const contract = new ethers.Contract(sushiMaster,masterChef,provider);
 
-    const event = async function main() {
+const event = async function main() {
         
-        contract.on('Deposit', (user, pid, amount)=>{
-            insert('Deposit',user,pid,amount);
-        })
+    contract.on('Deposit', (user, pid, amount)=>{
+        insert('Deposit',user,pid,amount);
+    })
     
-        contract.on('Withdraw', (user, pid, amount)=>{
-            insert('Withdraw',user,pid,amount);
-        })
+    contract.on('Withdraw', (user, pid, amount)=>{
+        insert('Withdraw',user,pid,amount);
+    })
     
-        contract.on('EmergencyWithdraw', (user, pid, amount)=>{
-            insert('EmergencyWithdraw',user,pid,amount);
-        })
-    }
+    contract.on('EmergencyWithdraw', (user, pid, amount)=>{
+        insert('EmergencyWithdraw',user,pid,amount);
+    })
+}
 
-    function insert(eventType,user,pid,amount){
-        const Amount = amount.toNumber();
-        const Pid = pid.toNumber();
-        const event = new Event({
-            Event:eventType,
-            user:user,
-            poolId:Pid,
-            amount:Amount
-         });
-        event.save();    
-    }
+function insert(eventType,user,pid,amount){
+    const Amount = amount.toNumber();
+    const Pid = pid.toNumber();
+    const event = new Event({
+        Event:eventType,
+        user:user,
+        poolId:Pid,
+        amount:Amount
+    });
+
+    event.save();    
+}
+
 export default event;
